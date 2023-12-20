1d6dd1990070344f4c3d2c5ea8a1ac94fbd296d4
Simplexer <juhokim2018@gmail.com>
2023-12-19 22:18:11 -0800

    Created backtest

--- backtest.py
+++ backtest.py
@@ -1,77 +1,121 @@
+import config
+import csv
+from binance import Client
 import requests
 
-# Initialize a dataframe to keep track of transactions
-columns = ['timestamp', 'type', 'balance', 'crypto_value', 'gimp']
-transactions_df = pd.DataFrame(columns=columns)
+def get_binance_candlestick_data(symbol, interval, start_date, end_date):
+    # Initialize Binance client
+    client = Client(config.API_KEY, config.API_SECRET)
 
-def record_transaction(timestamp, transaction_type, balance, crypto_value, gimp):
-    global transactions_df
-    transactions_df = transactions_df.append({
-        'timestamp': timestamp,
-        'type': transaction_type,
-        'balance': balance,
-        'crypto_value': crypto_value,
-        'gimp': gimp
-    }, ignore_index=True)
+    # Get historical candlestick data
+    candles = client.get_historical_klines(symbol, interval, start_date, end_date)
 
-# Set initial balance and currency values
-balance = 1000
-currency = 1300
+    # Open CSV File
+    csvfile = open(f"binance_{symbol}.csv", 'w', newline='')
+    candlestick_writer = csv.writer(csvfile, delimiter=',')
+
+    # Add header
+    candlestick_writer.writerow([
+        "Kline open time",
+        "Open price",
+        "High price",
+        "Low price",
+        "Close price",
+        "Volume",
+        "Kline Close time",
+        "Quote asset volume",
+        "Number of trades",
+        "Taker buy base asset volume",
+        "Taker buy quote asset volume",
+        "Unused field, ignore"
+    ])
+
+    # Write information
+    for candlestick in candles:
+        candlestick[0] = candlestick[0] / 1000
+        candlestick_writer.writerow(candlestick)
+
+    csvfile.close()
+
+
+
+def get_upbit_candlestick(symbol, interval, start_date, end_date):
+    market = f"KRW-{symbol}"
+    url = f"https://api.upbit.com/v1/candles/{interval}?market={market}&count=1000&from={start_date}T00:00:00Z&to={end_date}T23:59:59Z"
+    print(url)
+
+    headers = {"accept": "application/json"}
+    
+    response = requests.get(url, headers=headers)
+    candlestick_data = response.json()
+
+    # Open CSV File
+    csvfile = open(f'upbit_{symbol}_{interval.replace("/", "")}.csv', 'w', newline='')
+    candlestick_writer = csv.writer(csvfile, delimiter=',')
+    
+    # Add header
+    candlestick_writer.writerow([
+        "market",
+        "candle_date_time_utc",
+        "candle_date_time_kst",
+        "opening_price",
+        "high_price",
+        "low_price",
+        "trade_price",
+        "timestamp",
+        "candle_acc_trade_price",
+        "candle_acc_trade_volume",
+        "unit"
+    ])
+    
+    # Write information
+    for candle in candlestick_data:
+        candlestick_writer.writerow([
+            candle["market"],
+            candle["candle_date_time_utc"],
+            candle["candle_date_time_kst"],
+            candle["opening_price"],
+            candle["high_price"],
+            candle["low_price"],
+            candle["trade_price"],
+            candle["timestamp"],
+            candle["candle_acc_trade_price"],
+            candle["candle_acc_trade_volume"],
+            candle["unit"]
+        ])
+    
+    csvfile.close()
+
+
+
+# # Sample initial values
+# balance = 1000
+# currency = "USD"
+# symbol_upbit = "XRP"
+# symbol_binance = "BTCUSDT"
+# interval_binance = "1d"
+# start_date = "2023-01-01"
+# end_date = "2023-12-31"
+# delay = 5  # in seconds
+
+
+
+# # Example usage:
+    
+# import time
+# start_time = time.time()
+# # get_binance_candlestick_data("BTCUSDT", Client.KLINE_INTERVAL_1MINUTE, "1 Jan, 2020", "2 Jan, 2020")
+# get_upbit_candlestick(symbol_upbit, "minutes/1", start_date, end_date)
+
+# print("--- %s seconds ---" % (time.time() - start_time))
+
+import pyupbit
 symbol = "BTC"
 
-# Get initial price difference across exchanges
-kimp, upbit, binance = kimpga(symbol)
 
-# Record initial state
-record_transaction(time.time(), 'initial', balance, balance * upbit, 0)
+# 끝나는 시간 입력 = UTC,  출력 = KST
+df = pyupbit.get_ohlcv(ticker=f"KRW-{symbol}", interval="minute1", count=10,to="2023-12-20 6:10:00")
+# 간단히 pyupbit.get_ohlcv("KRW-BTC", "day", 10)으로 써도 똑같이 동작
+print(df)
 
-# If KIMP (Binance -> Upbit)
-if kimp == "김프":
-    # Refresh price
-    binance_url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
-    binance_response = requests.get(binance_url)
-    binance = float(binance_response.json()["price"])
-
-    # Buy at Binance
-    balance -= binance * currency
-    record_transaction(time.time(), 'buy', balance, balance * binance, 1)
-
-    # Crypto transfer delay
-    time.sleep(10)
-
-    # Refresh price
-    upbit_url = f"https://api.upbit.com/v1/ticker?markets=KRW-{symbol}"
-    upbit_response = requests.get(upbit_url)
-    upbit = float(upbit_response.json()[0]["trade_price"])
-
-    # Sell at Upbit
-    balance += upbit
-    record_transaction(time.time(), 'sell', balance, balance * upbit, 1)
-
-# If ReverseKIMP (Upbit -> Binance)
-elif kimp == "역프":
-    # Refresh price
-    upbit_url = f"https://api.upbit.com/v1/ticker?markets=KRW-{symbol}"
-    upbit_response = requests.get(upbit_url)
-    upbit = float(upbit_response.json()[0]["trade_price"])
-
-    # Buy at Upbit
-    balance -= upbit
-    record_transaction(time.time(), 'buy', balance, balance * upbit, 0)
-
-    # Crypto transfer delay
-    time.sleep(10)
-
-    # Refresh price
-    binance_url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
-    binance_response = requests.get(binance_url)
-    binance = float(binance_response.json()["price"])
-
-    # Sell at Binance
-    balance += binance * currency
-    record_transaction(time.time(), 'sell', balance, balance * binance, 0)
-
-# Save transactions to CSV
-transactions_df.to_csv('transactions.csv', index=False)
-
-print("Transactions recorded successfully.")
+df.to_csv(f"upbit_{symbol}.csv")
