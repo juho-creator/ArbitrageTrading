6e0114a377ae1191bf0adf48c4366ee28e8335c3
Simplexer <juhokim2018@gmail.com>
2023-12-19 19:50:55 -0800

    updated layer1 and forward test

--- ForwardTest.py
+++ ForwardTest.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from Layer1_API import kimpga
+from Layer1_API import kimpga,get_currency
 import time
 import requests
 
@@ -7,78 +7,99 @@ import requests
 columns = ['timestamp', 'kimp', 'exchange', 'type', 'crypto_price', 'balance']
 transactions_df = pd.DataFrame(columns=columns)
 
+
+
+
+
 # Define a function to record transactions in the dataframe
 def record_transaction(timestamp, kimp, exchange, transaction_type, crypto_price, balance):
     global transactions_df
-    transactions_df = transactions_df.append({
-        'timestamp': timestamp,
-        'gimp': gimp,
-        'exchange': exchange,
-        'type': transaction_type,
-        'crypto_price': crypto_price,
-        'balance': balance
-    }, ignore_index=True)
+
+    # Create a new DataFrame for the new row
+    new_row = pd.DataFrame({
+        'timestamp': [timestamp],
+        'kimp': [kimp],
+        'exchange': [exchange],
+        'type': [transaction_type],
+        'crypto_price': [crypto_price],
+        'balance': [balance]
+    })
+
+    # Concatenate the new row DataFrame to the existing transactions_df
+    transactions_df = pd.concat([transactions_df, new_row], ignore_index=True)
+
+
+def forwardtest(balance, currency, symbol, delay):
+    # Get initial price difference across exchanges using the kimpga function
+    kimp, binance, upbit = kimpga(symbol,currency)
+    
+    # Record initial state
+    record_transaction(time.time(), kimp, 'Initial', binance, upbit, balance)
+
+    # If KIMP (Binance -> Upbit)
+    if kimp:
+        # Refresh price for Binance
+        binance_url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
+        binance_response = requests.get(binance_url)
+        binance = float(binance_response.json()["price"])
+
+        # Buy at Binance
+        balance -= binance * currency
+        record_transaction(time.time(), 1, 'Binance', 'buy', binance, balance)
+
+        # Crypto transfer delay
+        time.sleep(delay)
+
+        # Refresh price for Upbit
+        upbit_url = f"https://api.upbit.com/v1/ticker?markets=KRW-{symbol}"
+        upbit_response = requests.get(upbit_url)
+        upbit = float(upbit_response.json()[0]["trade_price"])
+
+        # Sell at Upbit
+        balance += upbit
+        record_transaction(time.time(), 1, 'Upbit', 'sell', upbit, balance)
+
+    # If ReverseKIMP (Upbit -> Binance)
+    elif not(kimp):
+        # Refresh price for Upbit
+        upbit_url = f"https://api.upbit.com/v1/ticker?markets=KRW-{symbol}"
+        upbit_response = requests.get(upbit_url)
+        upbit = float(upbit_response.json()[0]["trade_price"])
+
+        # Buy at Upbit
+        balance -= upbit
+        record_transaction(time.time(), 0, 'Upbit', 'buy', upbit, balance)
+
+        # Crypto transfer delay
+        time.sleep(10)
+
+        # Refresh price for Binance
+        binance_url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
+        binance_response = requests.get(binance_url)
+        binance = float(binance_response.json()["price"])
+
+        # Sell at Binance
+        balance += binance * currency
+        record_transaction(time.time(), 0, 'Binance', 'sell', binance, balance)
+
+    # Save transactions to CSV
+    transactions_df.to_csv('transactions.csv', index=False)
+
+    # Print success message
+    print("Transactions recorded successfully.")
 
 # Set initial balance and currency values
 balance = 1000
-currency = 1300
+currency = get_currency("USD")
 symbol = "XRP"
 delay = 20
 
-# Get initial price difference across exchanges using the kimpga function
-kimp, upbit, binance = kimpga(symbol)
 
-# Record initial state
-record_transaction(time.time(), gimp, 'Initial',binance, upbit, balance)
 
-# If KIMP (Binance -> Upbit)
-if kimp:
-    # Refresh price for Binance
-    binance_url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
-    binance_response = requests.get(binance_url)
-    binance = float(binance_response.json()["price"])
 
-    # Buy at Binance
-    balance -= binance * currency
-    record_transaction(time.time(), 1, 'Binance', 'buy', binance, balance)
 
-    # Crypto transfer delay
-    time.sleep(delay)
 
-    # Refresh price for Upbit
-    upbit_url = f"https://api.upbit.com/v1/ticker?markets=KRW-{symbol}"
-    upbit_response = requests.get(upbit_url)
-    upbit = float(upbit_response.json()[0]["trade_price"])
-
-    # Sell at Upbit
-    balance += upbit
-    record_transaction(time.time(), 1, 'Upbit', 'sell', upbit, balance)
-
-# If ReverseKIMP (Upbit -> Binance)
-elif not(kimp):
-    # Refresh price for Upbit
-    upbit_url = f"https://api.upbit.com/v1/ticker?markets=KRW-{symbol}"
-    upbit_response = requests.get(upbit_url)
-    upbit = float(upbit_response.json()[0]["trade_price"])
-
-    # Buy at Upbit
-    balance -= upbit
-    record_transaction(time.time(), 0, 'Upbit', 'buy', upbit, balance)
-
-    # Crypto transfer delay
-    time.sleep(10)
-
-    # Refresh price for Binance
-    binance_url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
-    binance_response = requests.get(binance_url)
-    binance = float(binance_response.json()["price"])
-
-    # Sell at Binance
-    balance += binance * currency
-    record_transaction(time.time(), 0, 'Binance', 'sell', binance, balance)
-
-# Save transactions to CSV
-transactions_df.to_csv('transactions.csv', index=False)
-
-# Print success message
-print("Transactions recorded successfully.")
+import time
+start_time = time.time()
+forwardtest(balance, currency, symbol, delay)
+print("--- %s seconds ---" % (time.time() - start_time))
--- Layer1_API.py
+++ Layer1_API.py
@@ -1,9 +1,40 @@
 import requests
+from bs4 import BeautifulSoup
 
-def kimpga(symbol):
-    # Initialize currency
-    currency = 1300.00  
+def get_currency(currency_code):
+    url = f'https://m.stock.naver.com/marketindex/exchange/FX_{currency_code}KRW'
+    headers = {'Accept-Language': 'ko-KR', 'User-Agent': 'Mozilla/5.0'}
 
+    # HTTP request & response from Naver
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parsing HTML response data with lxml for better performance
+        soup = BeautifulSoup(response.content, 'lxml')
+
+        # Extract exchange rate using a more specific selector
+        exchange_rate_tag = soup.select_one('strong.DetailInfo_price__I_VJn')
+
+        # Check if the tag is found
+        if exchange_rate_tag:
+            # Extract text from the tag and clean it up
+            exchange_rate = exchange_rate_tag.get_text(strip=True).replace("KRW", "").replace(",", "")
+
+            try:
+                # Convert the cleaned-up string to a float
+                exchange_rate = float(exchange_rate)
+                print(f"{currency_code}KRW = {exchange_rate}")
+                return exchange_rate
+            except ValueError:
+                print("Failed to convert exchange rate to float.")
+        else:
+            print("Exchange rate tag not found.")
+    else:
+        print(f"Failed to fetch data. Status Code: {response.status_code}")
+
+
+def kimpga(symbol,currency):
     try:
         # Fetch Binance price
         binance_url = f"https://api.binance.com/api/v3/ticker/price?symbol={symbol}USDT"
@@ -13,7 +44,7 @@ def kimpga(symbol):
         
         # Error Handling (Binance)
         if binance_close is not None:
-            print(f"Current price of {symbol} on Binance: $ {binance_close}")
+            print(f"{symbol} price on Binance: $ {binance_close} (₩ {round(binance_close*currency,2)})")
         else:
             print(f"No data received for {symbol} on Binance.")
             binance_close = None
@@ -26,7 +57,7 @@ def kimpga(symbol):
         # Error Handling (Upbit)
         if len(upbit_data) > 0:
             upbit_close = float(upbit_data[0]["trade_price"])
-            print(f"Current price of {symbol} on Upbit: ₩ {upbit_close}")
+            print(f"{symbol} price on Upbit: ₩ {upbit_close}")
         else:
             print(f"No data received for {symbol} on Upbit.")
             upbit_close = None
@@ -34,7 +65,7 @@ def kimpga(symbol):
         # Calculate Kimp
         if upbit_close is not None and binance_close is not None:
             calculated_value = ((upbit_close / (binance_close * currency)) - 1) * 100
-            print(f"Calculated Value for {symbol}: {round(calculated_value, 2)}")
+            print(f"Kimp {symbol}: {round(calculated_value, 2)} %")
             
             if calculated_value > 0:
                 return 1, binance_close, upbit_close  # 김프
@@ -49,7 +80,11 @@ def kimpga(symbol):
         print(f"Error in fetching data: {e}")
         return None
 
+# def main():
+#     kimp, binance, upbit = kimpga("XRP")
+#     print(kimp, binance, upbit)
 
-kimp, binance, upbit = kimpga("XRP")
-
-print(kimp, binance, upbit)
+# import time
+# start_time = time.time()
+# main()
+# print("--- %s seconds ---" % (time.time() - start_time))
