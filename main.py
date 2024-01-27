c55f9c65acbe0324c59d50d17c627f594be017af
Simplexer <72856990+juho-creator@users.noreply.github.com>
2024-01-28 04:01:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from module import *
 import time
 import signal
 import pyttsx3
-
+import json
+import pandas as pd
 
 engine = pyttsx3.init() # object creation
 
@@ -12,12 +13,14 @@ engine.setProperty('voice', voices[1].id)   #changing index, changes voices. 1 f
 
 
 
+
+
 # Auto Arbitrage Trading 
 def TransferArbitrage():
     # Initializing parameters
     symbol = "EOS"
     reverse_percent = 2.8
-    kimp_percent = 4
+    kimp_percent = 2
 
     # Get currency
     currency = get_currency_api("USD")
@@ -59,89 +62,34 @@ def TransferArbitrage():
         print("--- %s seconds ---" % (time.time() - start_time))
 
 
-# Check Triangular Arbitrage for a crypto 
-def checkTriangular(code="XRP"):
-    print(f"\n{code}")
-    a = upbit_price(f"KRW-{code}")
-    b = upbit_price(f"BTC-{code}")
-    c = upbit_price("KRW-BTC")
 
-    print(f"KRW-{code} : " + str(a))
-    print(f"BTC-{code} : " + str(b))
-    print("KRW-BTC : " + str(c))
+def TriangularArbitrage(symbol,exchange="upbit"):
+    one_way,profit,KRW_CODE,BTC_CODE,KRW_BTC = checkTriangular(symbol,exchange)
+    if profit > 1:
+        if one_way == True:
+            print("OneWay") 
+            OneWay(symbol,KRW_CODE,BTC_CODE,KRW_BTC)
 
-    d = round(b*c,2)
-
-    if a<d:
-        print(f"KRW-{code} ==> BTC-{code} ==> KRW-BTC")
-        profit = round((d-a)/a*100,2)
-        print(f"{a} ==> {d} ({profit}%)")
-        return True,profit
-    else:
-        print(f"BTC-{code} ==> KRW-BTC ==> KRW-{code}")
-        profit = round((a-d)/d*100,2)
-        print(f"{d} ==> {a} ({profit}%)")
-        return False,profit
+        elif one_way == False:
+            print("AnotherWay")
+            OtherWay(symbol,KRW_CODE,BTC_CODE,KRW_BTC)
 
 
-# Check Triangular Arbitrage for all crypto on upbit (10min)
-def check_all_triangular():
-
-    all = ['BTC', 'ETH', 'NEO', 'MTL', 'XRP', 'ETC', 'SNT', 'WAVES', 'XEM', 'QTUM', 'LSK', 'STEEM', 'XLM', 'ARDR', 'ARK', 'STORJ', 'GRS', 'ADA', 'SBD', 'POWR', 'BTG', 'ICX', 'EOS', 'TRX', 'SC', 'ONT', 'ZIL', 'POLYX', 'ZRX', 'LOOM', 'BCH', 'BAT', 'IOST', 'CVC', 'IQ', 'IOTA', 'HIFI', 'ONG', 'GAS', 'UPP', 'ELF', 'KNC', 'BSV', 'THETA', 
-    'QKC', 'BTT', 'MOC', 'TFUEL', 'MANA', 'ANKR', 'AERGO', 'ATOM', 'TT', 'CRE', 'MBL', 'WAXP', 'HBAR', 'MED', 'MLK', 'STPT', 'ORBS', 'VET', 'CHZ', 'STMX', 'DKA', 'HIVE', 'KAVA', 'AHT', 'LINK', 'XTZ', 'BORA', 'JST', 'CRO', 'TON', 'SXP', 'HUNT', 'PLA', 'DOT', 'MVL', 'STRAX', 'AQT', 'GLM', 'SSX', 'META', 'FCT2', 'CBK', 'SAND', 'HPO', 'DOGE', 'STRK', 'PUNDIX', 'FLOW', 'AXS', 'STX', 'XEC', 'SOL', 'MATIC', 'AAVE', '1INCH', 'ALGO', 'NEAR', 'AVAX', 'T', 'CELO', 'GMT', 'APT', 'SHIB', 'MASK', 'ARB', 'EGLD', 'SUI', 'GRT', 'BLUR', 'IMX', 'SEI', 'MINA', 'CTC', 'ASTR']
-
-    all_profits = {}
-
-    for symbol in all: 
-        try:
-            direction,profit = checkTriangular(symbol)
-            all_profits[symbol] = [direction,profit]
-        except:
-            print(f"{symbol} : No triangular arbitrage")
-
-    sorted_dict = dict(sorted(all_profits.items(), key=lambda item: item[1][1], reverse=True))
-    pprint(sorted_dict, sort_dicts=False)
-    return sorted_dict
 
 
-# Execute Triangular Arbitrage Trading
-def TriangularArbitrage(symbol):
-    one_way = checkTriangular(symbol)
-    KRW = UpbitKRW()
 
-    if one_way == True: 
-        UBUY(f"KRW-{symbol}",KRW)
-
-        crypto = UCRYPTO(symbol)
     
-        USELL(f"BTC-{symbol}",crypto)
-
-        BTC = UCRYPTO("BTC")
-        USELL(f"KRW-BTC",BTC)
-
-    elif one_way == False:
-        UBUY(f"KRW-BTC",KRW)
-
-        BTC = UCRYPTO("BTC")
-        UBUY(f"BTC-{symbol}",BTC)
-
-        crypto = UCRYPTO(symbol)
-        USELL(f"KRW-{symbol}",crypto)
-
-
-
-
 start_time = time.time()
 
-arbitrage_opp = check_all_triangular()
+cryptos = upbit_ticker_all()
 
-test1 =  json.dumps(arbitrage_opp,indent=1)
 
-f = open("arbitrage_opp.txt", "w")
-f.write(str(test1))
-f.close()
+for crypto in cryptos:
+    print(crypto)
+    try:
+        upbitTriangular("BCH")
+    except:
+        print("Unable to compute arbitrage")
+    print()
 
 print("--- %s seconds ---" % (time.time() - start_time))
-
-
-
